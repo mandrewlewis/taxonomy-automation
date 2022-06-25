a56fd65007128100180f74a34d51195b2fc73fb5
mandrewlewis <81784056+mandrewlewis@users.noreply.github.com>
2022-06-24 20:09:00 -0700

    Hook up every step of automation and add logs
    Each step should now be included in the automation loop. CSV data
    should be imported properly. And now, when script is run, create
    txt file with timestamp that logs all automation steps for each loop.

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../Campus Sitemap & Taxonomy Work 2021-2022 - Wordpress Campus Sites.csv')
+df = pd.read_csv('Campus Sitemap & Taxonomy Work 2021-2022 - Wordpress Campus Sites.csv')
 
 clean_df = df.iloc[7::]
 clean_df = clean_df[['Unnamed: 0','Unnamed: 2','Unnamed: 10']]
@@ -10,19 +10,19 @@ clean_df = clean_df.dropna()
 
 
 
-print()
-for i in range(len(clean_df)):
-    print(f'GSuite row: {clean_df.iloc[i].name}')
+# print()
+# for i in range(len(clean_df)):
+#     print(f'GSuite row: {clean_df.iloc[i].name}')
 
-    tagSets = []
-    for tagSet in clean_df.iloc[i].Tags.split(','):
-        tagSets.append(tagSet.strip())
+#     tagSets = []
+#     for tagSet in clean_df.iloc[i].Tags.split(','):
+#         tagSets.append(tagSet.strip())
 
-    for tagSet in tagSets:
-        tags = tagSet.split('/')
-        print(tags)
+#     for tagSet in tagSets:
+#         tags = tagSet.split('/')
+#         print(tags)
 
-    print()
+#     print()
 
-print(f'Length = {len(clean_df)}')
-print()
+# print(f'Length = {len(clean_df)}')
+# print()
